a52119df86ffc8783906e52e7e54c48c609bed1f
mamanigrasso <m.manigrasso@outlook.com>
2018-04-10 19:03:58 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/mamanigrasso/PartyPoker""
    This reverts commit e215739dc0781f1e96cfa85fd22cf39b57afce1f.

---
+++ app/src/main/java/at/aau/pokerfox/partypoker/activities/GameActivity.java
@@ -0,0 +1,10 @@
+package at.aau.pokerfox.partypoker.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TimoS on 04.04.2018.
+ */
+
+public class GameActivity extends AppCompatActivity {
+}
