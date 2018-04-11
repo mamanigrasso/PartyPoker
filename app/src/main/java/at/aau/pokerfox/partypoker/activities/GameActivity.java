d66f274d9c3cf0690eadab213a7a7c451883f9f5
Timo Schmid <timo.schmid@gmx.at>
2018-04-11 16:20:30 +0200

    Imported carddesign

--- app/src/main/java/at/aau/pokerfox/partypoker/activities/GameActivity.java
+++ app/src/main/java/at/aau/pokerfox/partypoker/activities/GameActivity.java
@@ -1,10 +1,27 @@
 package at.aau.pokerfox.partypoker.activities;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import at.aau.pokerfox.partypoker.R;
 
 /**
  * Created by TimoS on 04.04.2018.
  */
 
 public class GameActivity extends AppCompatActivity {
-}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.layout_table);
+
+
+
+    }}
+
