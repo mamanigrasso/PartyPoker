791cf876c5b49e983110f1d7f2f8cf0a777106eb
Timo Schmid <timo.schmid@gmx.at>
2018-04-12 12:04:34 +0200

    schmid_fullscreen
    fullscreen
    pokertable buttons

--- app/src/main/java/at/aau/pokerfox/partypoker/activities/MainActivity.java
+++ app/src/main/java/at/aau/pokerfox/partypoker/activities/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         final Button btnHost = findViewById(R.id.btn_host);
