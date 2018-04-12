5c8bd92e89a616ad22d617a5b1dc2a11b5da1fdd
Umschaden Michael <Umschaden.Michael@gmx.at>
2018-04-12 08:04:06 +0200

    CardDeck Implement
    Enums for Card Deck

--- app/src/main/java/at/aau/pokerfox/partypoker/model/CardDeck.java
+++ app/src/main/java/at/aau/pokerfox/partypoker/model/CardDeck.java
@@ -3,7 +3,17 @@ package at.aau.pokerfox.partypoker.model;
 
 
 public class CardDeck {
+
     private static CardDeck DECK = new CardDeck();
+
+    public enum Suits{
+        CLUBS,HEARTS,DIAMONDS,SPADES
+    }
+
+    public enum Numerals{
+        DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
+        TEN, JACK(), QUEEN, KINGG, ACE;
+    }
 
     public static Card issueNextCardFromDeck() {
         return new Card((int)(1+Math.random()*3), (int)(4+Math.random()*10));
