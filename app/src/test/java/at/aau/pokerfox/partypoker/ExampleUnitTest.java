1739c84707135532047ae89074c0fc17511eef8d
andreaskog <andreaskog@edu.aau.at>
2018-05-15 18:00:43 +0200

    addedMockito to build.gradle to use Mockito für UnitTests

--- app/src/test/java/at/aau/pokerfox/partypoker/ExampleUnitTest.java
+++ app/src/test/java/at/aau/pokerfox/partypoker/ExampleUnitTest.java
@@ -1,14 +1,21 @@
 package at.aau.pokerfox.partypoker;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import android.content.SharedPreferences;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
